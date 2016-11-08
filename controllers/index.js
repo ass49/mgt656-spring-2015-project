a1c3e3fffc9ca2ca470b83d61993b25b9f3b8675
RAG <rebecca.ganster@yale.edu>
2016-11-08 19:31:00 +0000

    becca's changes

--- controllers/index.js
+++ controllers/index.js
@@ -4,10 +4,23 @@
  * Controller that renders our index (home) page.
  */
 function index (request, response) {
+  var now = new Date(); 
   var contextData = {
+<<<<<<< 59180c504c08a24649a540b1084b2dcccb558225
     'title': 'XCG Events',
     'tagline': 'Not campus groups'
+=======
+    'title': 'MGT 656',
+    'tagline': 'You are doomed (just kidding).',
+    'events': []
+>>>>>>> becca's changes
   };
+ for(var i=0; i < events.all.length; i++){
+   var event = events.all[i];
+   if(event.date > now){
+     contextData.events.push(event);
+   }
+ }
   response.render('index.html', contextData);
 }
 
